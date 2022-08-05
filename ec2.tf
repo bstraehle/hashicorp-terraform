153f4175e046c3d2edae540271a96b2e829a102d
bstraehle <50965537+bstraehle@users.noreply.github.com>
2022-08-05 16:13:51 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-2"
+  region     = "us-west-2"
   access_key = "<access-key>"
   secret_key = "<secret-key>"
 }
 
 resource "aws_instance" "web" {
-   ami = "ami-082b5a644766e0e6f"
+   ami           = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
 }
