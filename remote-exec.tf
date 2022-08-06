6aa67b81b9220a3a2b3b7044ae252beb3467a028
bstraehle <50965537+bstraehle@users.noreply.github.com>
2022-08-06 09:46:28 -0700

    Update remote-exec.tf

--- remote-exec.tf
+++ remote-exec.tf
@@ -1,25 +1,25 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "YOUR-KEY"
-  secret_key = "YOUR-KEY"
+  access_key = "<access-key>"
+  secret_key = "<secret-key>"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-0ca285d4c2cda3300"
-   instance_type = "t2.micro"
-   key_name = "terraform-key"
+resource "aws_instance" "web" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
 
-   connection {
-   type     = "ssh"
-   user     = "ec2-user"
-   private_key = file("./terraform-key.pem")
-   host     = self.public_ip
-   }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./terraform-key.pem")
+    host        = self.public_ip
+  }
 
- provisioner "remote-exec" {
-   inline = [
+  provisioner "remote-exec" {
+    inline = [
      "sudo amazon-linux-extras install -y nginx1",
      "sudo systemctl start nginx"
-   ]
- }
+    ]
+  }
 }
